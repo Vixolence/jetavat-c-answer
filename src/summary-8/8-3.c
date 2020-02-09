389b91006ec897b64d15d43561456a631f86f44a
Vixolence <knantajan@gmail.com>
2020-02-09 19:57:41 +0700

    Shorted the algorithm.

--- src/summary-8/8-3.c
+++ src/summary-8/8-3.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
-#define MINDIFF 2.25e-308
+#include <math.h>
 
-void main() {
-    int input;
-    double squareroot;
-    printf("Please fill your number : ");
-    scanf("%d", &input);
-    squareroot = sqrt(input);
-    printf("%f", sqrt);
-}
-
-double sqrt(double square) {
-    double root=square/3, last, diff=1;
-    if (square <= 0) return 0;
-    do {
-        last = root;
-        root = (root + square / root) / 2;
-        diff = root - last;
-    } 
-    while (diff > MINDIFF || diff < -MINDIFF);
-    return root;
+void main() 
+{
+    int num;
+    printf("Input number : ");
+    scanf("%d", &num);
+    printf("Result : %.3f", sqrt(num));
 }
