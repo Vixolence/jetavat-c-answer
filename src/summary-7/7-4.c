fc2e93e9e305ce16d5e2b74550047820631af41d
Vixolence <knantajan@gmail.com>
2020-02-09 19:57:02 +0700

    Shorted the algorithm and Removed some variables.

--- src/summary-7/7-4.c
+++ src/summary-7/7-4.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
-
-void main() {
-    int age[5];
-    double sum = 0;
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    double avg = 0;
-    int count = 0;
-
-    for (i = 0; i < 5; i++) {
-        count++;
-        printf("Please fill [%d] student age : ", count);
-        scanf("%d", &age[i]);
+void main()
+{
+    int student[5],i = 1, sum = 0;
+    for (int count = 0; count < 5; count++)
+    {
+        printf("Input [%d] student age : ", i++);
+        scanf("%d", &student[count]);
     }
-    
-    for (k = 0; k < 5; k++) {
-        sum += age[k];
-        j++;
-    }
-
-    avg = sum/j;
-
-    printf("Average age of %d students is : %f ", count, avg);
+    for (int arr_count = 0; arr_count < 5; arr_count++)
+        sum += student[arr_count];
+    double avg = sum / 5;
+    printf("Average of all student ages is %.3f", avg);
 }
