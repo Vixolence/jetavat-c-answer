5bf2f4bd07479cbec8aabb3e4bd5924caae53438
Vixolence <knantajan@gmail.com>
2020-02-09 19:51:14 +0700

    Reformatted and delete some brackets

--- src/summary-3/3-4.c
+++ src/summary-3/3-4.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
 void main () {
-    if ((10 != 5) && (7 <= 7)) {
+    if ((10 != 5) && (7 <= 7))
         printf("4.1 T\n");
-    }
-    else {
+    else
         printf("4.1 F\n");
-    }
 
-    if ((2 < 5) || (9 <= 7)) {
+    if ((2 < 5) || (9 <= 7)) 
         printf("4.2 T\n");
-    }
-    else {
+    else
         printf("4.2 F\n");
-    }
 
-    if ((8 == 8) && !(3 > 2)) {
+    if ((8 == 8) && !(3 > 2))
         printf("4.3 T\n");
-    }
-    else {
+    else 
         printf("4.3 F\n");
-    }
+    
 
 }
