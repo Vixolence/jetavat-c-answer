fa1abbbaea59372403b2630448bba42aa5e7380c
Vixolence <knantajan@gmail.com>
2020-02-10 18:20:49 +0700

    Sucess

--- src/summary-7/7-4.c
+++ src/summary-7/7-4.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 void main()
 {
-    int student[5],i = 1, sum = 0;
+    int student_age[5],i = 1, sum = 0;
     char student_name[10][10];
     for (int count = 0; count < 5; count++)
     {
         printf("Input [%d] student name : ",i);
-        scanf("%s",&student_name[count]);
+        scanf("%s",student_name[count]);
         printf("Input [%d] student age : ", i++);
         scanf("%d", &student_age[count]);
     }
