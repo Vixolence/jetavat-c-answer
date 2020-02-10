08deeb2ba71d38f83209680812149a288212f9e6
Vixolence <knantajan@gmail.com>
2020-02-10 18:18:19 +0700

    Gay

--- src/summary-7/7-4.c
+++ src/summary-7/7-4.c
@@ -6,12 +6,12 @@ void main()
     for (int count = 0; count < 5; count++)
     {
         printf("Input [%d] student name : ",i);
-        scanf("%s",&student[name]);
+        scanf("%s",&student_name[count]);
         printf("Input [%d] student age : ", i++);
-        scanf("%d", &student[count]);
+        scanf("%d", &student_count[count]);
     }
     for (int arr_count = 0; arr_count < 5; arr_count++)
-        sum += student[arr_count];
+        sum += student_age[arr_count];
     double avg = sum / 5;
     printf("Average of all student ages is %.3f", avg);
 }
