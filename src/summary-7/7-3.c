c94b57fc87eb7b6ca59054ed6d957b3be23d6325
Vixolence <knantajan@gmail.com>
2020-02-10 17:36:24 +0700

    Edit the wrong shit

--- src/summary-7/7-3.c
+++ src/summary-7/7-3.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 void main()
 {
-    int student[5],i = 1;
+    int student_age[5],i = 1;
+    char student_name[10][10];
     for (int count = 0; count < 5; count++)
     {
+        printf("Input [%d] student name : ",i);
+        scanf("%s", student_name[count]);
         printf("Input [%d] student age : ", i++);
-        scanf("%d", &student[count]);
+        scanf("%d", &student_age[count]);
+    }
+    
+    
+    /*for (int count = 0; count < 5; count++)
+    { 
+        printf("%d. %s Age = %d \n", count,student_name[count],student_age[count]);
     }
 }
+/*
